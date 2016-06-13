fd7408bddbaf2b3b774a4708b1201c0b3f175b00
sayito <say.edgar24@gmail.com>
2016-06-13 13:02:47 -0600

    actualizacion
    actualizacion

--- app/src/main/java/com/holamundo/diseno50/ceramicacaste/ProductoAdapter.java
+++ app/src/main/java/com/holamundo/diseno50/ceramicacaste/ProductoAdapter.java
@@ -1,10 +1,13 @@
 package com.holamundo.diseno50.ceramicacaste;
 
 import android.app.Activity;
+import android.content.res.Resources;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -37,11 +40,19 @@ public class ProductoAdapter extends ArrayAdapter<Producto> {
         TextView tDescripcion = (TextView) convertView.findViewById(R.id.textView4);
         TextView timage = (TextView) convertView.findViewById(R.id.textView5);
 
+        ImageView imgProducto= (ImageView) convertView.findViewById(R.id.imageView);
 
         tNombre.setText(producto.Nombre);
         tPrecio.setText(producto.Precio);
         tDescripcion.setText(producto.Descripcion);
         timage.setText(producto.image);
+
+        Resources res=convertView.getResources();
+        String mDrawableName= "cati";
+        int resId=res.getIdentifier(mDrawableName,"drawable","com.holamundo.diseno50.ceramicacaste");
+        imgProducto.setImageResource(resId);
+
+
         return convertView;
 
     }
---
+++ app/src/main/java/com/holamundo/diseno50/ceramicacaste/SignInActivity.java
@@ -0,0 +1,8 @@
+package com.holamundo.diseno50.ceramicacaste;
+
+/**
+ * Created by Diseño 50 on 13/06/2016.
+ */
+public class SignInActivity {
+
+}
